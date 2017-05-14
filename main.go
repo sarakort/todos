a93a0d0b9658610a53e3d62106a264ba88a5bc26
Chinnawat K <chinnawatsut@gmail.com>
2017-05-14 15:40:35 +0700

    packaging route

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"log"
-
 	"net/http"
+	"todo/route"
 
 	"github.com/ant0ine/go-json-rest/rest"
 )
@@ -12,35 +12,15 @@ func main() {
 	api := rest.NewApi()
 	api.Use(rest.DefaultDevStack...)
 	router, err := rest.MakeRouter(
-		rest.Get("/todos", GetTodos),
-		rest.Get("/todo/:id", GetTodo),
-		rest.Put("/todo/:id", PutTodo),
-		rest.Post("/todo", PostTodo),
-		rest.Delete("/todo/:id", DeleteTodo),
+		rest.Get("/todos", route.GetTodos),
+		rest.Get("/todo/:id", route.GetTodo),
+		rest.Put("/todo/:id", route.PutTodo),
+		rest.Post("/todo", route.PostTodo),
+		rest.Delete("/todo/:id", route.DeleteTodo),
 	)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	api.SetApp(router)
 	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
-}
-
-func GetTodos(w rest.ResponseWriter, r *rest.Request) {
-	w.WriteJson(nil)
-}
-
-func GetTodo(w rest.ResponseWriter, r *rest.Request) {
-
-}
-
-func PostTodo(w rest.ResponseWriter, r *rest.Request) {
-
-}
-
-func DeleteTodo(w rest.ResponseWriter, r *rest.Request) {
-
-}
-
-func PutTodo(w rest.ResponseWriter, r *rest.Request) {
-
 }
